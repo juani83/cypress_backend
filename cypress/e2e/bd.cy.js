4873f2d121374725a6c8eb1b0e6960f9f043c0c1
juani83 <juanbecerra83@gmail.com>
2023-09-05 15:47:54 -0300

    Probar con bases de Datos SQL

--- cypress/e2e/bd.cy.js
+++ cypress/e2e/bd.cy.js
@@ -1,7 +1,47 @@
-describe('Pruebas a bases de datos', () => {
-    it('Select', () => {
-        cy.task("queryDb", "SELECT * FROM pruebas").then(results => {
+describe('Pruebas a bases de datos', function() {
+    after(() => {
+        cy.task("queryDb", "DELETE FROM pruebas");
+    });
+
+    it('Insert', function() {
+        cy.task("queryDb", "INSERT INTO pruebas (nombre, apellidoMaterno, apellidoPaterno) VALUES ('Juan', 'Indart', 'Becerra')").then(results => {
             cy.log(results);
-        })
+            expect(results.affectedRows).to.eq(1);
+            cy.wrap(results.insertId).as("id");
+        });
+    });
+
+    it('Comprobar insert', function () {
+        cy.task("queryDb", `SELECT * FROM pruebas WHERE id=${this.id}`).then(results => {
+            cy.log(results);
+            expect(results[0].nombre).to.eq("Juan");
+            expect(results[0].apellidoMaterno).to.eq("Indart");
+            expect(results[0].apellidoPaterno).to.eq("Becerra");
+        });
+    });
+
+    it('Update nombre', function() {
+        cy.task("queryDb", `UPDATE pruebas SET nombre = "Ignacio" WHERE id=${this.id}`).then(results => {
+            cy.log(results);
+            expect(results.affectedRows).to.eq(1);
+            expect(results.serverStatus).to.eq(2);
+        });
+    });
+
+    it('Comprobar update', function () {
+        cy.task("queryDb", `SELECT * FROM pruebas WHERE id=${this.id}`).then(results => {
+            cy.log(results);
+            expect(results[0].nombre).to.eq("Ignacio");
+            expect(results[0].apellidoMaterno).to.eq("Indart");
+            expect(results[0].apellidoPaterno).to.eq("Becerra");
+        });
+    });
+
+    it('Delete', function() {
+        cy.task("queryDb", `DELETE FROM pruebas WHERE id=${this.id}`).then(results => {
+            cy.log(results);
+            expect(results.affectedRows).to.eq(1);
+            expect(results.serverStatus).to.eq(2);
+        });     
     });
 });
